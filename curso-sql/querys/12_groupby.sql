7e7f570d5da9e6ac49cb798379ae7d84cf761f13
gabriel-lumertz <glumertz10@gmail.com>
2026-01-14 22:36:12 -0300

    feat(sql): Add GROUP BY

---
+++ curso-sql/querys/12_groupby.sql
@@ -0,0 +1,17 @@
+SELECT
+    IdCliente,
+    sum(QtdePontos) AS QtdePontos,
+    count(IdTransacao) AS TotalTransacoes
+
+FROM
+    transacoes
+
+WHERE
+    DtCriacao >= '2025-07-01' AND
+    DtCriacao < '2025-08-01'
+
+GROUP BY
+    IdCliente
+
+ORDER BY
+    qtdePontos DESC;
